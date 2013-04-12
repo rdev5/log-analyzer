b20a48d6b92d7794066a23e2f3fc312ce9fb5fcc
rdev5 <rdev5@localhost>
2013-04-12 14:54:58 -0700

    Added function stream_file_line(). Ready to move functions into class file.

--- includes/functions.php
+++ includes/functions.php
@@ -1,7 +1,17 @@
 <?php
 /*
  * functions.php
+ *
+ * Note:
+ * Handling compressed files (*.gz) is 50% slower than handling uncompressed files
  */
+
+function microtime_benchmark($start_time)
+{
+
+  return microtime(true) - $start_time;
+
+}
 
 function gz_file($target_file) {
   
@@ -29,31 +39,82 @@ function open_file($target_file, $is_compressed = FALSE) {
   return $fp;
 }
 
-function stream_file($fp, $target_file, $is_compressed = FALSE, $limit = 5) {
+function stream_file($fp, $is_compressed = FALSE, $limit = 5, $suppress_output = FALSE) {
 
-  $l = 1;
+  $line_no = 1;
   $limit = (int)$limit;
+
   if($is_compressed) {
+
+    if(gztell($fp) != 0) rewind($fp);
 
     while( ($buffer = gzgets($fp)) !== false ) {
 
-      handle_line($l, $buffer);
+      if(!$suppress_output) handle_line($line_no, $buffer);
 
-      $l++;
-      if($l >= $limit) break;
+      $line_no++;
+      if($line_no > $limit) break;
 
     }
 
   } else {
 
+    if(ftell($fp) != 0) rewind($fp);
+
     while( ($buffer = fgets($fp)) !== false ) {
 
-      handle_line($l, $buffer);
+      if(!$suppress_output) handle_line($line_no, $buffer);
 
-      $l++;
-      if($l >= $limit) break;
+      $line_no++;
+      if($line_no > $limit) break;
+
     }
 
   }
+
+}
+
+function stream_file_line($l, $fp, $is_compressed, $suppress_output = FALSE) {
+
+  $line_no = 1;
+  $l = (int)$l;
+
+  if($l < 1) return FALSE;
+
+  if($is_compressed) {
+
+    if(gztell($fp) != 0) gzrewind($fp);
+
+    while( ($buffer = gzgets($fp)) !== false ) {
+
+      if($line_no == $l) {
+
+        if(!$suppress_output) handle_line($l, $buffer);
+        return gztell($fp);
+
+      }
+
+      $line_no++;
+    }
+
+  } else {
+
+    if(ftell($fp) != 0) rewind($fp);
+
+    while( ($buffer = fgets($fp)) !== false ) {
+
+      if($line_no == $l) {
+
+        if(!$suppress_output) handle_line($l, $buffer);
+        return ftell($fp);
+
+      }
+
+      $line_no++;
+    }
+
+  }
+
+  return FALSE;
 
 }
--- index.php
+++ index.php
@@ -10,8 +10,23 @@ function handle_line($l, $line) {
   echo "Line {$l}: {$line}<br /><br />";
 }
 
+$start_time = microtime(true);
+
 $is_compressed = gz_file($target_file);
 $fp = open_file($target_file, $is_compressed);
-stream_file($fp, $target_file, $is_compressed);
+
+echo '<h1>Log Analyzer</h1>';
+
+echo '<h2>stream_file()</h2>';
+stream_file($fp, $is_compressed);
+
+echo '<h2>stream_file_line()</h2>';
+for($i = 1; $i <= 600; $i++) {
+  $p = stream_file_line($i, $fp, $is_compressed);
+}
+
+echo '<hr />';
+echo 'Time: ' . microtime_benchmark($start_time) . 'ms<br />';
+echo "Pointer location: {$p}";
 
 fclose($fp);
